3e51bdf20c93ecdd9cb6e7f9d7e69359aa82bc32
daniela <danielamoreno699@gmail.com>
2023-10-11 12:58:40 -0500

    new tests

--- test.py
+++ test.py
@@ -1,35 +1,27 @@
-# import nltk
-# nltk.download()
-# from nltk.tokenize import sent_tokenize, word_tokenize
 
-# example_test= "Hello, There"
-# print(sent_tokenize(example_test))
-
+from flask import Flask, request, jsonify
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# Download VADER lexicon (if not already downloaded)
 nltk.download('vader_lexicon')
-
-# Initialize the sentiment analyzer
-sia = SentimentIntensityAnalyzer()
-
-# Sample text for sentiment analysis
 text = "I do not like it."
 
-# Analyze the sentiment of the text
-sentiment = sia.polarity_scores(text)
-print(sentiment)
-if sentiment['compound'] > 0:
-    print('this is positive')
-elif sentiment['compound'] < 0:
-    print('this is negative')
-else: 
-    print('neither positive or negative')
+def SentimentAnalyzer(str):
 
-# Interpret the sentiment scores
-print("Sentiment Scores:")
-print("Positive: ", sentiment['pos'])
-print("Negative: ", sentiment['neg'])
-print("Neutral: ", sentiment['neu'])
-print("Compound: ", sentiment['compound'])
+    response = ''
+    sia = SentimentIntensityAnalyzer()
+# Analyze the sentiment of the text
+    sentiment = sia.polarity_scores(text)
+
+    if sentiment['compound'] > 0:
+        response = 'this is positive'
+        print('this is positive')
+    elif sentiment['compound'] < 0:
+        response = 'this is negative'
+        print('this is negative')
+    else: 
+        response = 'this is neutral'
+        print('neutral')
+    return response
+SentimentAnalyzer(text)
+
