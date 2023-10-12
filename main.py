66c9fc289117a0f6b2987a1eac357f9b5d300364
daniela <danielamoreno699@gmail.com>
2023-10-11 19:29:58 -0500

    modify code

--- main.py
+++ main.py
@@ -1,49 +1,36 @@
-import requests
 from flask import Flask, request, jsonify
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
-#text = "I do not like it."
 
-# sentiment analizer function for api
-def SentimentAnalyzer(str):
-
-    response = ''
-    sia = SentimentIntensityAnalyzer()
-    sentiment = sia.polarity_scores(str)
-
-    if sentiment['compound'] > 0:
-        response = 'this is positive'
-        print('this is positive')
-    elif sentiment['compound'] < 0:
-        response = 'this is negative'
-        print('this is negative')
-    else: 
-        response = 'this is neutral'
-        print('neutral')
-    return response
-#SentimentAnalyzer(text)
-
-# route
 app = Flask(__name__)
 
-@app.route('/sentiment', methods=['POST'])
+# Define the sentiment analyzer function
+def SentimentAnalyzer(text):
+    sia = SentimentIntensityAnalyzer()
+    sentiment = sia.polarity_scores(text)
+
+    if sentiment['compound'] > 0:
+        response = 'This is positive'
+    elif sentiment['compound'] < 0:
+        response = 'This is negative'
+    else:
+        response = 'This is neutral'
+    return response
+
+
+@app.route('/sentiment', methods=['GET'])
 def analyze_sentiment():
-    if request.method == 'POST':
-        data = request.get_json()
-        text = data.get('text')
+    text = request.args.get('q')  
 
-        if text:
-            sia_response = SentimentAnalyzer(text)
-            result = {'result': sia_response}
-            return jsonify(result)
-        else:
-            return jsonify({'error': 'Please provide a "text" parameter in the request.'}), 400
-        
- 
+    if text:
+        sia_response = SentimentAnalyzer(text)
+        result = {'result': sia_response}
+        return jsonify(result)
+    else:
+        return jsonify({'error': 'Please provide a "q" query parameter in the URL.'}), 400
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
