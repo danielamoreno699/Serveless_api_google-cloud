636c067242027c46c4e20e08b43f5de5cff84e75
daniela <danielamoreno699@gmail.com>
2023-10-12 15:08:13 -0500

    change port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -33,4 +34,6 @@ def analyze_sentiment():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+    # Use PORT environment variable if available, otherwise default to 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
